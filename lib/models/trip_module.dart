110b9a334e32c0a2720d6b392acc52f48ef3cb2f
Oferd <oferdrr100@gmail.com>
2021-03-16 22:15:25 +0200

    update

--- lib/models/trip_module.dart
+++ lib/models/trip_module.dart
@@ -1,1 +1,17 @@
-class Trip {}
+
+class Trip {
+
+ double lat ;
+ double lng ;
+ String name;
+
+
+ Trip({this.lat,this.lng,this.name});
+ 
+ Map<String, dynamic> toJson() => {
+        'lat': lat,
+        'lng': lng,
+        'name':name
+      };
+
+}
